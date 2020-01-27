99da973a0cab54a00b71d147860dd8a6814a8c24
DonovanZhu <49983377+DonovanZhu@users.noreply.github.com>
2020-01-26 22:24:35 -0500

    Add files via upload

---
+++ RP.py
@@ -0,0 +1,41 @@
+import socket
+import threading
+import msgpack
+import time
+from collections import deque
+
+motors_que = deque(maxlen=4)
+motor_q = deque()
+
+
+def udp_send(udp_socket):
+    i = 0
+    num1 = '160.39.222.206'
+    num2 = 1000
+    while True:
+        send_data = msgpack.packb(i)
+        udp_socket.sendto(send_data, (num1, num2))
+        i += 1
+        time.sleep(1)
+
+
+def udp_recv(udp_socket):
+    while True:
+        recv_data = udp_socket.recv(1024)
+        motors_que.append(msgpack.unpackb(recv_data))
+        print('Deque:%s' % motors_que)
+
+
+def main():
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    ip = '160.39.222.206'
+    port = 1001
+    udp_socket.bind((ip, port))
+    t = threading.Thread(target=udp_recv, args=(udp_socket,))
+    t1 = threading.Thread(target=udp_send, args=(udp_socket,))
+    t.start()
+    t1.start()
+
+
+if __name__ == '__main__':
+    main()
