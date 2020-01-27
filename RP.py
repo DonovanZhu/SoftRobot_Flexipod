9408372629114cbf6831f5b2e115dde226c37220
DonovanZhu <49983377+DonovanZhu@users.noreply.github.com>
2020-01-27 09:43:59 -0500

    Update RP.py

--- RP.py
+++ RP.py
@@ -8,18 +8,18 @@ motors_que = deque(maxlen=4)
 motor_q = deque()
 
 
-def udp_send(udp_socket):
+def UdpSendToPC(udp_socket, ip_remote, port_remote):
     i = 0
-    num1 = '160.39.222.206'
-    num2 = 1000
+    # In each second, this function sends a rotation angle data to PC.py.
     while True:
         send_data = msgpack.packb(i)
-        udp_socket.sendto(send_data, (num1, num2))
+        udp_socket.sendto(send_data, (ip_remote, port_remote))
         i += 1
         time.sleep(1)
 
 
-def udp_recv(udp_socket):
+def UdpRecvFromPC(udp_socket):
+    # In this function, it receives velocity command from PC.py.
     while True:
         recv_data = udp_socket.recv(1024)
         motors_que.append(msgpack.unpackb(recv_data))
@@ -27,14 +27,19 @@ def udp_recv(udp_socket):
 
 
 def main():
+    ip_local = '160.39.222.206'
+    port_local = 1001
+
+    ip_remote = '160.39.222.206'
+    port_remote = 1000
+
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    ip = '160.39.222.206'
-    port = 1001
-    udp_socket.bind((ip, port))
-    t = threading.Thread(target=udp_recv, args=(udp_socket,))
-    t1 = threading.Thread(target=udp_send, args=(udp_socket,))
-    t.start()
-    t1.start()
+    udp_socket.bind((ip_local, port_local))
+
+    thread_sender = threading.Thread(target=UdpSendToPC, args=(udp_socket, ip_remote, port_remote))
+    thread_receiver = threading.Thread(target=UdpRecvFromPC, args=(udp_socket,))
+    thread_sender.start()
+    thread_receiver.start()
 
 
 if __name__ == '__main__':
