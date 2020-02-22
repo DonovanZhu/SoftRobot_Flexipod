7ab91e2df77184291418018ff5d52607b8b5331f
DonovanZhu <49983377+DonovanZhu@users.noreply.github.com>
2020-02-22 14:28:59 -0500

    Update RPi_UDP_Receiver.py

--- Transport between PC & RPi/RPi_UDP_Receiver.py
+++ Transport between PC & RPi/RPi_UDP_Receiver.py
@@ -1,120 +1,97 @@
 #!/usr/bin/env python
 # coding: utf-8
-"""
-This file is for testing the brushless DC motor: DJI M2006 P36 and its controller: DJI C610. Raspberry Pi 4 is used to send to and receive message from controller. Since Raspberrypi has no CAN bus interface,  a CANable pro chip is used to transfer message between CAN and serial. To know more information of DJI motor and its controller:
-https://www.robomaster.com/zh-CN/products/components/general/M2006
-To understand more about script: https://python-can.readthedocs.io/en/master/
-For more information of CANable: https://canable.io/getting-started.html
-"""
-
 from __future__ import print_function
-from binascii import hexlify
-import time
+import socket
 import threading
+import msgpack
+import time
+from collections import deque
 import can
 from can import Message
-from collections import deque
+from binascii import hexlify
 
-k_p = 1.0
-k_i = 0.0
-k_d = 0.0
-motors_que = deque(maxlen=2)
-speedDirectionBoundary = 32768
-maxBoundary = 65536
-drive_ratio = 36
-PID_H = 100
-PID_L = -100
+motors_que = deque()
+motor_q = deque()
+
+
+def UdpSendToPC(udp_socket, ip_remote, port_remote):
+    global motor_q
+    while True:
+        if motor_q:
+            motor_data = motor_q.popleft()
+            send_data = msgpack.packb(motor_data)
+            udp_socket.sendto(send_data, (ip_remote, port_remote))
+
+
+def UdpRecvFromPC(udp_socket):
+    # In this function, it receives velocity command from PC.py.
+    global motors_que
+    while True:
+        recv_data = udp_socket.recv(1024)
+        motors_que.append(msgpack.unpackb(recv_data))
+        #print(msgpack.unpackb(recv_data))
 
 
 def send_cyclic(bus, msg, stop_event):
     """The loop for sending."""
     global motors_que
-    desire_speed = 60
     start_time = time.time()
-    while True:
-        if len(motors_que) > 1:
-            break
-    former_msg = motors_que.popleft()
-    former_speed = str(hexlify(former_msg.data), "utf-8")
-    former_speed = int(former_speed[4:8], 16)
-    if former_speed >= speedDirectionBoundary:
-        former_speed = (maxBoundary - former_speed) / drive_ratio
-    former_error = desire_speed - former_speed
-    error = former_error
-
     while not stop_event.is_set():
-        new_msg = motors_que.popleft()
-        dt = new_msg.timestamp - former_msg.timestamp
-
-        new_speed = str(hexlify(new_msg.data), "utf-8")
-        new_speed = int(new_speed[4:8], 16)
-        if new_speed >= speedDirectionBoundary:
-            new_speed = (maxBoundary - former_speed) / drive_ratio
-        new_error = desire_speed - new_speed
-
-        error += new_error
-        v_command = k_p * new_error + k_i * error * dt + k_d * (new_error - former_error) / dt
-
-        former_msg = new_msg
-        former_error = new_error
-        former_speed = new_speed
-
-        if v_command > 0:
-            if v_command > PID_H:
-                v_command = PID_H
-            current_command_H, current_command_L = divmod(int((v_command / PID_H) * (speedDirectionBoundary - 1)), 0x100)
-            msg.data[0] = current_command_H
-            msg.data[1] = current_command_L
-        elif v_command < 0:
-            if v_command < PID_L:
-                v_command = PID_L
-            current_command = int((v_command / PID_L) * (speedDirectionBoundary - 1))
-            current_command += 0x8000
-            current_command_H, current_command_L = divmod(current_command, 0x100)
-            msg.data[0] = current_command_H
-            msg.data[1] = current_command_L
-
         msg.timestamp = time.time() - start_time
         # send message to bus
+        if len(motors_que) != 0:
+            motor_command = motors_que.popleft()
+            motor_command = bytes.decode(motor_command)
+            print(motor_command)
+            for i in range(0, len(motor_command), 2):
+                #msg.data[int(i / 2)] = bytes.fromhex(motor_command[i:i+2])
+                msg.data[int(i / 2)] = int(motor_command[i:i+2], 16)
+            #print(msg.data)
+
         bus.send(msg)
 
         # improve the current 1 unit every one second
-        time.sleep(0.1)
+        #print(f"tx: {msg.data}")
+        time.sleep(0.5)
 
 
 def receive(bus, stop_event):
-    global motors_que
     """The loop for receiving."""
+    global motor_q
     while not stop_event.is_set():
-        Motor_msg = bus.recv()
-        motors_que.append(Motor_msg)
-
-
-def pid(dt, desired_speed, msg, former_error, error):
-    msg_data = str(hexlify(msg.data), "utf-8")
-    msg_data = int(msg_data[4:8], 16)
-    if msg_data >= 32768:
-        msg_data = 65536 - msg_data
-    v_error = desired_speed - msg_data / 36
-    error += v_error
-    v_command = k_p * v_error + k_i * error * dt + k_d * (v_error - former_error) / dt
-
-    return v_command
+        rx_msg = bus.recv()
+        #data = bytes.decode(data)
+        msg_sending = str(hexlify(rx_msg.data), "utf-8")
+        if rx_msg is not None and len(msg_sending) == 16:
+            if rx_msg.arbitration_id == 513:
+                msg_sending += '1'
+            elif rx_msg.arbitration_id == 514:
+                msg_sending += '2'
+            elif rx_msg.arbitration_id == 515:
+                msg_sending += '3'
+            elif rx_msg.arbitration_id == 516:
+                msg_sending += '4'
+            print(msg_sending)
+            #msg_sending = hex(int(msg_sending, 16))
+            motor_q.append(msg_sending)
+            #print("rx: {}".format(rx_msg.data))
 
 
 def main():
+    ip_local = '192.168.0.27'
+    port_local = 1001
+
+    ip_remote = '192.168.0.231'
+    port_remote = 1000
+
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_socket.bind((ip_local, port_local))
     """Control the sender and receiver."""
     with can.interface.Bus(bustype='slcan', channel='/dev/ttyACM0', bitrate=1000000) as bus:
         tx_msg: Message = can.Message(
             # controller can id 0x200
             arbitration_id=0x200,
             data=[0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
-            # Every two hex number stand for the high and low byte of current
-            # data[0] + data[1] = 0x0000, which means the current is 0x0000
-            # Smaller and equal to 0x7FFF, rotating counter clockwise
-            # Larger and equal to 0x8000,rotating clockwise
-            # When current is 0x0000 or 0xffff, the motor is stop
-            # from -10A ~ 10A
             is_extended_id=False
         )
 
@@ -122,8 +99,11 @@ def main():
         stop_event = threading.Event()
 
         t_send_cyclic = threading.Thread(target=send_cyclic, args=(bus, tx_msg, stop_event))
-
         t_receive = threading.Thread(target=receive, args=(bus, stop_event))
+        thread_sender = threading.Thread(target=UdpSendToPC, args=(udp_socket, ip_remote, port_remote))
+        thread_receiver = threading.Thread(target=UdpRecvFromPC, args=(udp_socket,))
+        thread_sender.start()
+        thread_receiver.start()
         t_receive.start()
         t_send_cyclic.start()
 
@@ -137,5 +117,5 @@ def main():
     time.sleep(0.5)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
