e554e199c91a08bd525c76a7c01ccbfe3fd1ac1e
DonovanZhu <49983377+DonovanZhu@users.noreply.github.com>
2020-03-04 15:00:06 -0500

    Create CTest.c

---
+++ BLDC_Motor/CTest.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+
+#include <linux/can.h>
+#include <linux/can/raw.h>
+
+int main(int argc, char **argv)
+{
+	int s, i; 
+	int nbytes;
+	struct sockaddr_can addr;
+	struct ifreq ifr;
+	struct can_frame frame;
+
+	printf("CAN Sockets Receive Demo\r\n");
+
+	if ((s = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) 
+	{
+		perror("Socket");
+		return 1;
+	}
+	
+	printf("%d\n",socket(PF_CAN, SOCK_RAW, CAN_RAW));
+	strcpy(ifr.ifr_name, "can0" );
+	ioctl(s, SIOCGIFINDEX, &ifr);
+
+	memset(&addr, 0, sizeof(addr));
+	addr.can_family = AF_CAN;
+	addr.can_ifindex = ifr.ifr_ifindex;
+	
+	if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0) 
+	{
+		perror("Bind");
+		return 1;
+	}
+
+	while(1)
+	{
+		double start = clock();
+		nbytes = read(s, &frame, sizeof(struct can_frame));
+
+		if (nbytes < 0) 
+		{
+			perror("Read");
+			return 1;
+		}
+
+		printf("0x%03X \n",frame.can_id);
+
+		for (i = 0; i < frame.can_dlc; i++)
+			printf("%02X ",frame.data[i]);
+		double end = clock();
+		printf("\t time = %f\n", (double)(end - start) / CLOCKS_PER_SEC);
+	}
+	
+	printf("\r\n");
+
+	if (close(s) < 0) 
+	{
+		perror("Close");
+		return 1;
+	}
+
+	return 0;
+}
