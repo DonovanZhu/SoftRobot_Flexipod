879111f231d6a12633150be6527cb52d0697f8e0
DonovanZhu <49983377+DonovanZhu@users.noreply.github.com>
2020-04-02 18:33:43 -0400

    Create TeensyTest.ino

---
+++ TransportByCpp/TeensyTest.ino
@@ -0,0 +1,19 @@
+byte mes[2] = {0x01, 0x02};
+
+void setup()
+{
+  delay(3000);
+  // put your setup code here, to run once:
+  Serial.begin(2000000);           //  setup serial
+  // while (! Serial);
+  //sets the maximum milliseconds to wait for serial data
+  Serial.setTimeout(1);
+  Serial.flush();
+
+}
+void loop()
+{
+  Serial.write(mes, sizeof(mes));
+  Serial.flush();
+  delay(200);
+}
