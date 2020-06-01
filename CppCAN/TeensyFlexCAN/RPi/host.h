5729412a0a1d47f572c2a542fc5f26dc2a301279
DonovanZhu <49983377+DonovanZhu@users.noreply.github.com>
2020-05-31 21:42:13 -0400

    Update host.h

--- CppCAN/TeensyFlexCAN/RPi/host.h
+++ CppCAN/TeensyFlexCAN/RPi/host.h
@@ -1,7 +1,3 @@
-/*
- *  Definitions for host.c
- */
- 
 #ifndef __HOST_H
 #define __HOST_H
 
@@ -12,8 +8,8 @@
 #define HOST_ERROR_WRITE_SER  -4        // Write error on serial
 #define HOST_ERROR_BAD_PK_SZ  -5        // Bad incoming packet size error
 #define HOST_ERROR_MAGIC      -6        // Bad magic number received
-#define NB_ESC             4                 // Number of ESCs
-#define MAX_ESC            4                 // Max number of ESCs
+#define NB_ESC				   4                 // Number of ESCs
+#define MAX_ESC                4                 // Max number of ESCs
 
 #define USB_UART_SPEED     115200            // Baudrate of the teeensy USB serial link
 
@@ -22,28 +18,24 @@
 // Teensy->host communication data structure
 // sizeof(ESCPID_comm)=64 to match USB 1.0 buffer size
 typedef struct {
-  uint32_t      magic;                        // Magic number
-  uint8_t       deg[MAX_ESC];          // ESC temperature (°C)
-  int16_t       rpm[MAX_ESC];          // Motor rpm (10 rpm)
-  uint16_t      amp[MAX_ESC];          // ESC current (0.01A)
+	uint32_t	magic;                        // Magic number
+	uint16_t	deg[MAX_ESC];          // ESC temperature (°C)
+	uint16_t	rpm[MAX_ESC];          // Motor rpm (10 rpm)
+	uint16_t	amp[MAX_ESC];          // ESC current (0.01A)
 } Teensycomm_struct_t;
 
 // Host->teensy communication data structure
 // sizeof(Host_comm)=64 to match USB 1.0 buffer size
 typedef struct {
-  uint32_t      magic;                        // Magic number
-  int16_t       RPM[MAX_ESC];        // Velocity reference (10 rpm)
+	uint32_t	magic;                        // Magic number
+	int16_t		RPM[MAX_ESC];        // Velocity reference (10 rpm)
 } RPicomm_struct_t;
 
-
 // Prototypes
-char *Host_name_from_serial(  uint32_t );
-int   Host_get_fd(            uint32_t );
-int   Host_init_port(         uint32_t );
-void  Host_release_port(      uint32_t );
-int   Host_comm_update(       uint32_t, 
-                              int16_t*, 
-                              Teensycomm_struct_t** );
-                              
+char *Host_name_from_serial(uint32_t);
+int   Host_get_fd(uint32_t);
+int   Host_init_port(uint32_t);
+void  Host_release_port(uint32_t);
+int   Host_comm_update(uint32_t, int16_t*, Teensycomm_struct_t**);
 
 #endif
