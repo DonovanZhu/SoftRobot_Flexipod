7dc256f5a3c9da0040777251e44d8fe4b625a85c
DonovanZhu <49983377+DonovanZhu@users.noreply.github.com>
2020-06-07 10:29:55 -0400

    Update TeensyCAN.h

--- CppCAN/TeensyFlexCAN/Teensy/TeensyCAN.h
+++ CppCAN/TeensyFlexCAN/Teensy/TeensyCAN.h
@@ -3,7 +3,6 @@
 
 // Defines
 #define NB_ESC             4                 // Number of ESCs
-#define MAX_ESC            4                 // Max number of ESCs
 
 #define USB_UART_SPEED     115200            // Baudrate of the teeensy USB serial link
 
@@ -16,16 +15,18 @@
 // sizeof(Teensy_comm)=64 to match USB 1.0 buffer size
 typedef struct {
   uint32_t      magic;                        // Magic number
-  int       deg[MAX_ESC];          // Motors rotation angle(°C)
-  int       rpm[MAX_ESC];          // Motors rpm
-  int       amp[MAX_ESC];          // Motors current
+  int           deg[NB_ESC];          // Motors rotation angle(°C)
+  int           rpm[NB_ESC];          // Motors rpm
+  int           amp[NB_ESC];          // Motors current
+  double        acc[3];
+  double        gyr[3];
 } Teensycomm_struct_t;
 
 // Host->teensy communication data structure
 // sizeof(RPi_comm)=64 to match USB 1.0 buffer size
 typedef struct {
   uint32_t      magic;                        // Magic number
-  double        RPM[MAX_ESC];        // Velocity reference (10 rpm)
+  double        RPM[NB_ESC];        // Velocity reference (10 rpm)
 } RPicomm_struct_t;
 
 #endif
