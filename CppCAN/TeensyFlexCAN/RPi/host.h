d2d3ab8e304cd55d94480cda4663c7ca8f380bc6
DonovanZhu <49983377+DonovanZhu@users.noreply.github.com>
2020-06-07 10:28:56 -0400

    Update host.h

--- CppCAN/TeensyFlexCAN/RPi/host.h
+++ CppCAN/TeensyFlexCAN/RPi/host.h
@@ -8,27 +8,28 @@
 #define HOST_ERROR_WRITE_SER  -4        // Write error on serial
 #define HOST_ERROR_BAD_PK_SZ  -5        // Bad incoming packet size error
 #define HOST_ERROR_MAGIC      -6        // Bad magic number received
-#define NB_ESC				   4                 // Number of ESCs
-#define MAX_ESC                4                 // Max number of ESCs
+#define NB_ESC				         4        // Number of ESCs
 
-#define USB_UART_SPEED     115200            // Baudrate of the teeensy USB serial link
+#define USB_UART_SPEED        115200       // Baudrate of the teeensy USB serial link
 
-#define COMM_MAGIC         0x43305735        // Magic number: "teensy35" in leet speech
+#define COMM_MAGIC            0x43305735   // Magic number: "teensy35" in leet speech
 
 // Teensy->host communication data structure
 // sizeof(ESCPID_comm)=64 to match USB 1.0 buffer size
 typedef struct {
-  uint32_t      magic;                        // Magic number
-  int       	deg[MAX_ESC];          // Motors rotation angle(°C)
-  int       	rpm[MAX_ESC];          // Motors rpm
-  int			amp[MAX_ESC];          // Motors current
+  uint32_t    magic;                // Magic number
+  int       	deg[NB_ESC];          // Motors rotation angle(°C)
+  int       	rpm[NB_ESC];          // Motors rpm
+  int			    amp[NB_ESC];          // Motors current
+  double    	acc[3];
+  double    	gyr[3];
 } Teensycomm_struct_t;
 
 // Host->teensy communication data structure
 // sizeof(RPi_comm)=64 to match USB 1.0 buffer size
 typedef struct {
-  uint32_t      magic;                        // Magic number
-  double       	RPM[MAX_ESC];        // Velocity reference (10 rpm)
+  uint32_t      magic;              // Magic number
+  double       	RPM[NB_ESC];        // Velocity reference (10 rpm)
 } RPicomm_struct_t;
 
 // Prototypes
