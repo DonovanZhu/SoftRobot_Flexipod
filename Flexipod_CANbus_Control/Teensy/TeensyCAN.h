90d20ea62f578ea247642d33587999ae693cb5ac
DonovanZhu <49983377+DonovanZhu@users.noreply.github.com>
2020-06-09 00:59:05 -0400

    Update TeensyCAN.h

--- Flexipod_CANbus_Control/Teensy/TeensyCAN.h
+++ Flexipod_CANbus_Control/Teensy/TeensyCAN.h
@@ -3,28 +3,32 @@
 
 // Defines
 #define NB_ESC             4                 // Number of ESCs
+#define USB_UART_SPEED     2000000            // Baudrate of the teeensy USB serial link
+//
+// if speed command higher than 32768, the motor rotates clockwise
+// if speed command lower than 32768, the motor rotates counter-clockwise
+//
+#define DirectionBoundary  32768.0   // 32768(dec) = 0x8000(hex)
+#define maxBoundary        65535.0   // 0xffff, command upper limit
+#define PID_H              10000.0   // PID controller output upper limit, corresponding to 10A
+#define PID_L              -10000.0  // PID controller output lower limit,
+#define DRIVE_RATIO        36.0      // Drive ratio of motor gear box
+#define k_p                26.0      // Proportional parameter
+#define k_i                0.040     // Integral parameter
 
-#define USB_UART_SPEED     115200            // Baudrate of the teeensy USB serial link
-
-#define COMM_MAGIC         0x43305735        // Magic number: "teensy35" in leet speech
-#define RESET_GAIN         0xffff            // PIDf gain value that triggers teensy reset
-
-#define ERROR_MAGIC        -1                // Magic number error code
 
 // Teensy->host communication data structure
 typedef struct {
-  uint32_t      magic;                // Magic number
-  int           deg[NB_ESC];          // Motors rotation angle(°C)
-  int           rpm[NB_ESC];          // Motors rpm
-  int           torque[NB_ESC];       // Motors current
-  double        acc[3];               // Acceleration in X Y Z direction, m/s^2
-  double        gyr[3];               // Gyroscope in X Y Z direction, deg/s
+  double        angle[NB_ESC];       // Motors rotation angle, deg
+  double        rspeed[NB_ESC];      // Motors rotation speed, rpm
+  double        torque[NB_ESC];      // Motors torque, N*m
+  double        acc[3];              // Acceleration in X Y Z direction, m/s^2
+  double        gyr[3];              // Gyroscope in X Y Z direction, deg/s
 } Teensycomm_struct_t;
 
 // Host->teensy communication data structure
 typedef struct {
-  uint32_t      magic;                // Magic number
-  double        RPM[NB_ESC];          // Desired Speed, rpm
+  double        desired_speed[NB_ESC];          // Desired Speed, rpm
 } RPicomm_struct_t;
 
 #endif
