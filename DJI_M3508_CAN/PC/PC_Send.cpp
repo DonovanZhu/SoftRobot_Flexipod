a8ee63621a3dc02ba059cd66f2779c5e705072b6
DonovanZhu <49983377+DonovanZhu@users.noreply.github.com>
2020-06-18 10:11:47 -0400

    Create PC_Send.cpp

---
+++ DJI_M3508_CAN/PC/PC_Send.cpp
@@ -0,0 +1,153 @@
+#include <iostream>
+#include <iomanip>
+#include <WS2tcpip.h>
+#include <string>
+#include <windows.h>
+#include <conio.h>
+#include <vector>
+#include <string>
+#include <msgpack.hpp>
+
+
+// Include the Winsock library (lib) file
+#pragma comment (lib, "ws2_32.lib")
+
+// Saves us from typing std::cout << etc. etc. etc.
+using namespace std;
+
+class MotorSpeed {
+public:
+	double rpm[4];
+	MSGPACK_DEFINE(rpm);
+};
+
+MotorSpeed RotationSpeed;
+
+void main()
+{
+	WSADATA data;
+
+	WORD version = MAKEWORD(2, 2);
+
+	int wsOk = WSAStartup(version, &data);
+	if (wsOk != 0)
+	{
+		// Not ok! Get out quickly
+		cout << "Can't start Winsock! " << wsOk;
+		return;
+	}
+
+	sockaddr_in server;
+	server.sin_family = AF_INET; // AF_INET = IPv4 addresses
+	server.sin_port = htons(1000); // Little to big endian conversion
+	inet_pton(AF_INET, "192.168.0.77", &server.sin_addr); // Convert from string to byte array
+
+	SOCKET out = socket(AF_INET, SOCK_DGRAM, 0);
+
+	char s[4] = { 0.0, 0.0, 0.0, 0.0 };
+
+	double motor1_command = 0.0, motor2_command = 0.0, motor3_command = 0.0, motor4_command = 0.0;
+	int kbinput;
+	int speedcommand;
+	double speed;
+	vector<MotorSpeed> SendCommand;
+	LARGE_INTEGER freq, start, stop;
+	int j = 0;
+	while (true)
+	{
+		//QueryPerformanceFrequency(&freq);
+
+		//QueryPerformanceCounter(&start);
+		// cout << "enter the number:" << endl;
+		// cout << (int16_t)s[0] << endl;
+		// cin >> speed_command;
+		
+		if (_kbhit())
+		{
+			kbinput = _getch();
+			if (kbinput == 'q')
+				motor1_command += 10.0;
+			else if (kbinput == 'a')
+				motor1_command -= 10.0;
+			else if (kbinput == 'w')
+				motor2_command += 10.0;
+			else if (kbinput == 's')
+				motor2_command -= 10.0;
+			else if (kbinput == 'e')
+				motor3_command += 10.0;
+			else if (kbinput == 'd')
+				motor3_command -= 10.0;
+			else if (kbinput == 'r')
+				motor4_command += 10.0;
+			else if (kbinput == 'f')
+				motor4_command -= 10.0;
+			else if (kbinput == 'z')
+			{
+				motor1_command = 0.0;
+				motor2_command = 0.0;
+				motor3_command = 0.0;
+				motor4_command = 0.0;
+			}
+			else if (kbinput == 'x')
+			{
+				motor1_command += 10.0;
+				motor2_command += 10.0;
+				motor3_command -= 10.0;
+				motor4_command -= 10.0;
+			}
+			else if (kbinput == 'c')
+			{
+				motor1_command -= 10.0;
+				motor2_command -= 10.0;
+				motor3_command += 10.0;
+				motor4_command += 10.0;
+			}
+		}
+		/*
+		if (j > 1000 && j <= 2000)
+		{
+			motor1_command = 0.0;
+		}
+		else if (j >= 0 && j <= 1000)
+		{
+			motor1_command = 200.0;
+		}
+		*/
+		if (j > 2000)
+		{
+			system("cls");
+			j = 0;
+		}
+		j++;
+		RotationSpeed.rpm[0] = motor1_command;
+		RotationSpeed.rpm[1] = motor2_command;
+		RotationSpeed.rpm[2] = motor3_command;
+		RotationSpeed.rpm[3] = motor4_command;
+		SendCommand.clear();
+		SendCommand.push_back(RotationSpeed);
+
+		msgpack::sbuffer sbuf;
+		msgpack::pack(sbuf, SendCommand);
+		for (int i = 0; i < 4; i++)
+		{
+			printf("%f", RotationSpeed.rpm[i]);
+			printf("	");
+			//cout << "0x" << setfill('0') << setw(2) << hex << (int16_t)(unsigned char)s[i] << " ";
+		}
+		printf("\n");
+		//Sleep(1);
+		sendto(out, sbuf.data(), sbuf.size(), 0, (sockaddr*)&server, sizeof(server));
+
+
+		//QueryPerformanceCounter(&stop);
+		//double timeused = (double)(stop.QuadPart - start.QuadPart) / (double)freq.QuadPart;
+		//cout << timeused << endl;
+		//cout << endl;
+		// cout << x << endl;
+		// command += 1.0;
+	}
+	//Sleep(10);
+	closesocket(out);
+
+	WSACleanup();
+}
