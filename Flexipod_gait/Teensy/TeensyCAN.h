017f3fd789b3b028b8fccabb434ce921e3e2724f
Hongbo Zhu <49983377+DonovanZhu@users.noreply.github.com>
2020-08-25 16:56:40 -0400

    Update TeensyCAN.h

--- Flexipod_gait/Teensy/TeensyCAN.h
+++ Flexipod_gait/Teensy/TeensyCAN.h
@@ -19,6 +19,17 @@
 #define k_i                0.15       // Integral parameter
 #define k_p_ang            5.0
 
+// -0.0192715 -0.2800474 10.0537329 0.0136186 0.0011329 -0.0076148
+#define GRAVITY 9.802 // The gravity acceleration in New York City
+
+// Calibration outcomes
+#define GYRO_X_OFFSET 0.0131094
+#define GYRO_Y_OFFSET 0.0012340
+#define GYRO_Z_OFFSET -0.0056364
+
+#define ACCEL_X_OFFSET 0.0426984
+#define ACCEL_Y_OFFSET -0.3458546
+#define ACCEL_Z_OFFSET 10.0628948
 
 
 // Teensy->host communication data structure
@@ -29,6 +40,9 @@ typedef struct {
   double  motor_command[NB_ESC];    // Motors speed command
   double  acc[3];             // Acceleration in X Y Z direction, m/s^2
   double  gyr[3];             // Gyroscope in X Y Z direction, deg/s
+  double  mag[3];
+  double  eular[3];
+  double  timestamps;
 } Teensycomm_struct_t;
 
 // Host->teensy communication data structure
